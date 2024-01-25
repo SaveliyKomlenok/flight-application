10850021b6131773df527c5056c4234e96c0bc15
Saveliy Komlenok <skomlenok363@gmail.com>
2024-01-25 11:35:53 +0300

    Created project and added database connection

---
+++ src/main/java/by/saveliykomlenok/JdbcRunner.java
@@ -0,0 +1,13 @@
+package by.saveliykomlenok;
+
+import by.saveliykomlenok.utils.ConnectionManager;
+
+import java.sql.SQLException;
+
+public class JdbcRunner {
+    public static void main(String[] args) throws SQLException {
+        try (var connection = ConnectionManager.open()) {
+            System.out.println(connection.getTransactionIsolation());
+        }
+    }
+}
